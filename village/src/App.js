0e931f10f9d6edf4a6781b6f67b3baa8e97352d9
Olivia Osborn <olioz95@gmail.com>
2019-01-25 12:22:01 -0500

    added routes and nav links for home and smurf-form

--- village/src/App.js
+++ village/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from "axios";
+import { Route, NavLink} from "react-router-dom";
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -32,8 +33,23 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <NavLink exact to="/">Smurf List</NavLink>
+        <NavLink to="/smurf-form">Smurf Form</NavLink>
+        <Route
+          exact path="/"
+          render={props =>
+            <Smurfs
+              {...props}
+              smurfs={this.state.smurfs}
+            />}
+        />
+        <Route 
+          path="/smurf-form"
+          render={props =>
+            <SmurfForm
+              {...props}
+            />}
+        />
       </div>
     );
   }
